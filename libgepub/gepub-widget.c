e080b6824c27c78d28df3a932f5b952f923c9231
Daniel García Moreno <danigm@wadobo.com>
2018-08-09 09:44:44 +0200

    widget updated with the rust lib prefix

--- libgepub/gepub-widget.c
+++ libgepub/gepub-widget.c
@@ -256,8 +256,8 @@ resource_callback (WebKitURISchemeRequest *request, gpointer user_data)
       return;
 
     uri = g_strdup (webkit_uri_scheme_request_get_uri (request));
-    // removing "epub:///"
-    path = uri + 8;
+    // removing "epub://"
+    path = uri + 7;
     contents = gepub_doc_get_resource (widget->doc, path);
     mime = gepub_doc_get_resource_mime (widget->doc, path);
 
