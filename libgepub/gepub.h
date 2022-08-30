7634e86936ff27396ffa6b68ad7ff0b60e4fbec4
Daniel García Moreno <dani@danigm.net>
2022-08-30 09:57:52 +0200

    Make gepub-widget configurable
    This makes the WebKit2 dep optional so the library can be used just to
    read epubs and do not require the whole webkit when it's not needed.

--- libgepub/gepub.h
+++ libgepub/gepub.h
@@ -1,9 +1,14 @@
 #ifndef _GEPUB__H_
 #define _GEPUB__H_
 
+#include <config.h>
+
 #include "gepub-archive.h"
 #include "gepub-text-chunk.h"
 #include "gepub-doc.h"
+
+#ifdef GEPUB_WIDGET_ENABLED
 #include "gepub-widget.h"
+#endif
 
 #endif
